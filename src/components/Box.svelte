91c8e0f9e7f430c21c325e00ac838587f6fe3300
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-21 17:52:02 -0400

    Replaced meth with box

---
+++ src/components/Box.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div>
+	<slot />
+</div>
+
+<style lang="less">
+	@to: #dadada;
+
+	div {
+		background-color: @to;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		border-radius: 53px;
+		padding: 8px;
+	}
+</style>
