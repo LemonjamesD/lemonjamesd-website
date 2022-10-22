84017480082c75b2eeb1d1e2330c84eb07f48f08
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-22 08:43:07 -0400

    Added color gradient and float option

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+	export let float: string | undefined = undefined;
 </script>
 
-<div>
+<div style="float: {float}">
 	<slot />
 </div>
 
 <style lang="less">
-	@to: #dadada;
-
 	div {
-		background-color: @to;
+		background: linear-gradient(
+			0deg,
+			rgba(182, 197, 198, 1) 0%,
+			rgba(203, 215, 215, 1) 100%
+		);
 		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 		border-radius: 53px;
 		padding: 8px;
+		margin: 4px;
 	}
 </style>
