37a701245784dca9d286d3d5d787e9e66dbc7875
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-25 18:59:49 -0400

    Reset Prokect

--- src/components/BoxNav.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Box from "./Box.svelte";
-	export let page = "/";
-	export let className: string | undefined = undefined;
-</script>
-
-<a href={page}>
-	<Box {className} float="left"><slot /></Box>
-</a>
