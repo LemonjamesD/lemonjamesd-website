b1a08ac0a5ceffaa8497c07ce16a405eeae9e9a2
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-27 16:03:09 -0400

    Added FloatingSign

---
+++ src/components/FloatingSign.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	export let rotation = 0;
+
+	function cssVariables(node, variables) {
+		setCssVariables(node, variables);
+
+		return {
+			update(variables) {
+				setCssVariables(node, variables);
+			},
+		};
+	}
+	function setCssVariables(node, variables) {
+		for (const name in variables) {
+			node.style.setProperty(`--${name}`, variables[name]);
+		}
+	}
+</script>
+
+<div use:cssVariables={{ rotation }}>
+	<p>
+		<slot />
+	</p>
+</div>
+
+<style lang="less">
+	div {
+		transform: rotate(calc(var(--rotation) * 1deg));
+
+		background-color: rgb(204, 255, 255);
+
+		border-radius: 32px;
+		padding: 1px;
+
+		width: fit-content;
+	}
+
+	p {
+		padding: 8px;
+
+		background-color: rgb(165, 213, 224);
+
+		border-radius: 16px;
+		margin: 8px;
+
+		width: fit-content;
+	}
+</style>
