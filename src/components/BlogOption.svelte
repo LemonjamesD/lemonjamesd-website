d42c945adc40f8a37e308ad4d9ec0b0bfd31726a
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-28 19:18:50 -0400

    Made Blogs

---
+++ src/components/BlogOption.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import BoxNav from "./BoxNav.svelte";
+
+	export let page: string;
+	export let title: string;
+	export let date: string;
+</script>
+
+<BoxNav {page}>
+	<center><h1>{title}</h1></center>
+	<center><h5>{"Date Created: " + date}</h5></center>
+	<center><slot /></center>
+	<br />
+</BoxNav>
+
+<style lang="less">
+	center {
+		align-items: center;
+		align-content: center;
+	}
+</style>
